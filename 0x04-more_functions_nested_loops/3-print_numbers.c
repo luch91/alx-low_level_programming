a3dd4cf32dfcc863c9b6194c73abea9aed13d1f5
luch91 <luchijudith@gmail.com>
2022-10-20 21:35:41 -0700

    made corrections

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	char c;
+	int a = 0;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar ('\n');
+	while (a < 10)
+
+		_putchar(a++ + '0');
+
+	_putchar('\n');
 }
